70e84a3029e779aa196dd596aa763a1d4d4909d0
usmansaleem90 <98312186+usmansaleem90@users.noreply.github.com>
2024-09-12 11:42:10 -0700

    Update Card.jsx

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,12 +1,30 @@
 import { Link } from "react-router-dom";
 
 const Card = ({ icon, text, to, color, onClick }) => {
+  
   return (
-    <Link
+   <>
+    <style>
+        {`
+
+          @media (max-width: 400px) {
+            .custom-card {
+              width: 130px;
+            }
+          }
+            @media (max-width: 340px and min-width:280px) {
+            .custom-card {
+              width: 100px;
+            }
+          }
+        `}
+      </style>
+   
+   <Link
       onClick={onClick}
       style={{ color: color }}
       to={to}
-      className={`bg-white  shadow-2xl text-gradient-1  rounded-lg  overflow-hidden hover:scale-105 transition-all p-2 md:w-[30%] lg:w-[20%] w-[160px] h-[130px] hover:bg-blue-300 hover:text-white`}
+      className={`bg-white  shadow-2xl text-gradient-1 custom-card  rounded-lg  overflow-hidden hover:scale-105 transition-all p-2 md:w-[30%] lg:w-[20%]   w-[160px] h-[130px] hover:bg-blue-300 hover:text-white`}
     >
       <div className=" flex flex-col  items-center">
         <div className="flex justify-end  w-full ">
@@ -16,7 +34,7 @@ const Card = ({ icon, text, to, color, onClick }) => {
         <img src={icon} alt="" />
         <p className={`text-lg font-semibold mt-3 text-blu-300`}>{text} </p>
       </div>
-    </Link>
+    </Link></>
   );
 };
 
